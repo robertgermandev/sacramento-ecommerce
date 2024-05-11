aa4464baddb83d02c6dd82108ac5c22cc1b5e59f
robertgerman <robertgerman578@yahoo.ro>
2024-05-11 17:13:46 +0300

    Added header, directory components and categories design

---
+++ src/components/Directory/index.js
@@ -0,0 +1,25 @@
+import React from "react";
+import ShopSailBoats from "../../assets/shopSailBoats.jpg";
+import ShopYachts from "../../assets/shopYacths.jpg";
+import "./styles.scss";
+
+const Directory = (props) => {
+  return (
+    <div className="directory">
+      <div className="wrap">
+        <div
+          className="item"
+          style={{ backgroundImage: `url(${ShopSailBoats})` }}
+        >
+          <a>Sail Boats</a>
+        </div>
+
+        <div className="item" style={{ backgroundImage: `url(${ShopYachts})` }}>
+          <a>Yachts</a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Directory;
